e4c97be8e49631f181e5f915fc4080703864049a
Cristopher Ricardo Erazo Vallejos <cerazova@peralba.phys.sissa.it>
2026-01-25 15:09:30 +0100

    plots different configurations

--- shell/loop_config.sh
+++ shell/loop_config.sh
@@ -25,10 +25,10 @@ lr=0.05
 
 # Loop over various configurations = sigma, amp, type_enc,  unmb
 configurations=(
-    "0.1 1.0 wave False" # Unbalanced with wave encodings
-    # "0.5 0.5 wave False" # Balanced with wave encodings
-    # "0.5 1.0 learned False" # learned encodings
-    # "0.5 1.0 learned True" # Learned encodings and untied unembedddings
+    # "0.1 1.0 wave False" # Unbalanced with wave encodings
+    "0.5 0.5 wave False" # Balanced with wave encodings
+    "0.5 1.0 learned False" # learned encodings
+    "0.5 1.0 learned True" # Learned encodings and untied unembedddings
 )
 
 for config in "${configurations[@]}"; do
